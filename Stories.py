37abcebbb0d360923b38f3196d5df70f60cca2d1
james <jameshinns@gmail.com>
2023-10-06 19:32:46 +0200

    Support for SKLearn Models other than treebased models added, PyDoc, Temperature Parameter.

--- Stories.py
+++ Stories.py
@@ -1,16 +1,30 @@
-from abc import ABC, abstractmethod
 import pandas as pd
-
 import shap
-
 import openai
 
-# SHAP and CFstory should inherit from common parent XAIstory
-
 class SHAPstory():
+  """
+    A class to generate SHAPstories, narratives that explain AI predictions based on SHAP values.
+    
+    Attributes:
+    -----------
+    api_key : str
+        The OpenAI API key for generating narratives.
+    feature_desc_df : DataFrame
+        A DataFrame containing descriptions for each feature.
+    dataset_description : str
+        A brief description of the dataset.
+    input_description : str
+        Description of the input features.
+    target_description : str
+        Description of the target variable.
+    gpt_model : str, default="gpt-4"
+        The model to be used from OpenAI.
+    """
 
   def __init__(self, api_key, feature_desc_df, dataset_description, 
-               input_description, target_description,gpt_model = "gpt-4"):
+               input_description, target_description, gpt_model = "gpt-4"):
+    """Initializes the SHAPstory class with necessary parameters."""
     self.api_key = api_key
     self.feature_desc_df = feature_desc_df
     self.dataset_description = dataset_description
@@ -18,23 +32,59 @@ class SHAPstory():
     self.target_description = target_description
     self.gpt_model = gpt_model
 
-  def gen_shap_feature_df(self,x,model):
+  def gen_shap_feature_df(self, x, model, tree):
+    """
+    Generates a DataFrame with original features and their corresponding SHAP values.
+    
+    Parameters:
+    -----------
+    x : DataFrame
+        The input data for which SHAP values are to be calculated.
+    model : object
+        A trained model which supports SHAP explanations.
+    tree : bool, default=True
+        Boolean indicating if the model is tree-based. If True, TreeExplainer will be 
+        used for SHAP explanations, else KernelExplainer will be used.
+    
+    Returns:
+    --------
+    DataFrame
+        A DataFrame with original features and their SHAP values.
+    """
 
-    explainer = shap.TreeExplainer(model)
-
-    shap_vals = explainer.shap_values(x)[1].T
+    if tree:
+      explainer = shap.TreeExplainer(model)
+      shap_vals = explainer.shap_values(x)[1].T
+    else:
+      explainer = shap.KernelExplainer(model.predict, x)
+      shap_vals = explainer.shap_values(x).T
 
     df_copy = x.copy()
 
     for i,col in enumerate(df_copy.columns):
-      df_copy[col + " SHAP Value"] = shap_vals[i]
+      df_copy[f"{col} SHAP Value"] = shap_vals[i]
 
     return df_copy
 
-  def gen_variables(self,model,x,y):
+  def gen_variables(self,model,x,y,tree):
+    """
+    Generate necessary variables including SHAP values and predictions.
     
+    Parameters:
+    -----------
+    model : object
+        A trained model which supports SHAP explanations.
+    x : DataFrame
+        The input data.
+    y : Series or array-like
+        The true labels for the input data.
+    tree : bool, default=True
+        Boolean indicating if the model is tree-based. If True, TreeExplainer will be 
+        used for SHAP explanations, else KernelExplainer will be used.
+    """
+
     # Generate table with shap values for all instances given
-    self.shap_feature_df = self.gen_shap_feature_df(x,model)
+    self.shap_feature_df = self.gen_shap_feature_df(x,model,tree)
 
     # Generate predictions and scores
     y_pred   = model.predict(x)
@@ -46,7 +96,20 @@ class SHAPstory():
       "score" : y_scores
     })
 
-  def generate_prompt(self,iloc_pos):
+  def generate_prompt(self, iloc_pos):
+    """
+    Generates the prompt for OpenAI's GPT model to generate a narrative.
+    
+    Parameters:
+    -----------
+    iloc_pos : int
+        Index position of the instance for which the prompt is generated.
+        
+    Returns:
+    --------
+    str
+        The generated prompt.
+    """
 
     prediction, ground_truth, score = self.result_df.iloc[iloc_pos]
   
@@ -86,7 +149,23 @@ class SHAPstory():
 
     return prompt_string
 
-  def generate_response(self, prompt):
+  def generate_response(self, prompt, temp):
+    """
+    Gets the response from the OpenAI's GPT model based on a given prompt.
+    
+    Parameters:
+    -----------
+    prompt : str
+        The generated prompt for which the narrative is to be generated.
+    temp : float, default=0.2
+        The temperature setting for the GPT model.
+        
+    Returns:
+    --------
+    Response object
+        The response from OpenAI's API.
+    """
+
     openai.api_key = self.api_key
 
     message=[{"role": "user", "content": prompt}]
@@ -94,20 +173,38 @@ class SHAPstory():
     response = openai.ChatCompletion.create(
         model=self.gpt_model,
         messages = message,
-        temperature=0.2,
+        temperature=temp,
         max_tokens = 1000
     )
 
-    return response
+    return response.choices[0]["message"]["content"]
 
-  def generate_stories(self,model,x,y):
-    self.gen_variables(model,x,y)
+  def generate_stories(self,model,x,y,temp=0.2,tree=True):
+    """
+    Generates SHAPstories for each instance in the given data.
+    
+    Parameters:
+    -----------
+    model : object
+        A trained model which supports SHAP explanations.
+    x : DataFrame
+        The input data.
+    y : Series or array-like
+        The true labels for the input data.
+    temp : float, default=0.2
+        The temperature setting for the GPT model.
+    tree : bool, default=True
+        Boolean indicating if the model is tree-based. If True, TreeExplainer will be 
+        used for SHAP explanations, else KernelExplainer will be used.
+    
+    Returns:
+    --------
+    list of str
+        A list containing the generated SHAPstories for each instance.
+    """
 
-    stories = []
+    self.gen_variables(model, x, y, tree)
 
-    for i in range(len(x)):
-      prompt = self.generate_prompt(i)
-      response = self.generate_response(prompt)
-      stories.append(response.choices[0]["message"]["content"])
+    stories = [self.generate_response(self.generate_prompt(i), temp) for i in range(len(x))]
 
     return stories
